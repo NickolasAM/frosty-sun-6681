401e1a3cf479b6ff6d9b673e08b98e1706e01920
Nickolas Marquez <nickomarquez7@gmail.com>
2023-04-24 11:55:18 -0600

    Delete hospital doctor join tabel

--- db/migrate/20230424154226_create_hospital_doctors.rb
+++ db/migrate/20230424154226_create_hospital_doctors.rb
@@ -1,6 +1,7 @@
 class CreateHospitalDoctors < ActiveRecord::Migration[5.2]
   def change
     create_table :hospital_doctors do |t|
+      drop_table :hospital_doctors
       t.references :hospital, foreign_key: true
       t.references :doctor, foreign_key: true
 
